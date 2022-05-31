bbb5b6286d86639d406e0e81cb04349022831389
mankic <miic0803@gmail.com>
2022-05-31 22:21:33 +0900

    수정 - 필요없는 코드 삭제, TextField 사용

--- blog/models.py
+++ blog/models.py
@@ -2,22 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    class Meta:
-        db_table = "category"
-
     name = models.CharField(max_length=256)
-    desc = models.CharField(max_length=256)
+    desc = models.TextField()
 
 
 class Article(models.Model):
-    class Meta:
-        db_table = "article"
-
     title = models.CharField(max_length=256)
+    content = models.TextField()
+    # category에는 Category object가 들어가야한다.
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    body = models.CharField(max_length=256)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+
 
 
 
