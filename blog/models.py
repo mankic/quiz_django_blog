cbd9bd4952ca9abff0f11754bdab32bdabcb7d68
mankic <miic0803@gmail.com>
2022-05-31 10:21:40 +0900

    추가 - category와 article 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+
+    name = models.CharField(max_length=256)
+    desc = models.CharField(max_length=256)
+
+
+class Article(models.Model):
+    class Meta:
+        db_table = "article"
+
+    title = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    body = models.CharField(max_length=256)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+
