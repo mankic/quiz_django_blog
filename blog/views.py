b4f6b738eb49f46b4da31bcf19bea78616af64ea
mankic <miic0803@gmail.com>
2022-05-31 22:24:14 +0900

    추가 - 게시글 쓸 수 있는 페이지와 기능 함수

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Article
 
 # Create your views here.
+def new_view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title',None)
+        content = request.POST.get('content',None)
+        category = request.POST.get('category',None)
+        # html에서 입력받은 값이므로 category가 string형식
+
+        # category object 형식으로 저장
+        category = Category.objects.get(name=category)
+        article = Article.objects.create(title=title, content=content, category=category)
+
+        # 자동으로 pk 라는 필드를 만들어준다.
+        # 첫번째 글을 썼다 -> pk = 1
+        return redirect('/detail', article.pk)
+    elif request.method == 'GET':
+        # 페이지 받아오면서 현 Category 모델에 존재하는 것만 사용자가 선택해야해서, 전체 불러온다.
+        categories = Category.objects.all()
+        return render(request, 'new.html',{'categories':categories})
