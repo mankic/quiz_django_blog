f1bdf4efbd020a3d08902348abcf0b288e85e7c0
mankic <miic0803@gmail.com>
2022-06-01 20:25:27 +0900

    수정 - 데이터베이스 테이블 이름 추가

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        db_table = "category"
     name = models.CharField(max_length=256)
     desc = models.TextField()
 
 
 class Article(models.Model):
+    class Meta:
+        db_table = "article"
     title = models.CharField(max_length=256)
     content = models.TextField()
     # category에는 Category object가 들어가야한다.
